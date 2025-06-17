949550c97198e95a158db7c900493475b17980b8
Sunil Dutt <sunil.dutt28@gmail.com>
2025-06-17 23:44:52 +0100

    Homework for the linear regression

--- src/02 Linear regression.jl
+++ src/02 Linear regression.jl
@@ -16,9 +16,12 @@ models(filter)
 models("XGB")
 measures("F1")
 
-mdls = models(matching(X, y))
+
 
 # Linear regression
+
+using Pkg    
+Pkg.add("MLJLinearModels")
 
 LR = @load LinearRegressor pkg = MLJLinearModels
 
@@ -47,6 +50,8 @@ data = coerce(data, autotype(data, :discrete_to_continuous))
 y = data.MedV
 X = select(data, Not(:MedV))
 
+mdls = models(matching(X, y))
+
 # Let's declare a simple multivariate linear regression model:
 
 model = LR()
@@ -56,6 +61,8 @@ model = LR()
 X_uni = select(X, :LStat) # only a single feature
 mach_uni = machine(model, X_uni, y)
 fit!(mach_uni)
+ŷ = MLJ.predict(mach_uni, X_uni)
+round(rsquared(ŷ, y), sigdigits=4)
 
 # You can then retrieve the  fitted parameters using `fitted_params`:
 
@@ -145,3 +152,189 @@ plot!(Xnew.LStat, MLJ.predict(mach, Xnew), linewidth=3, color=:orange)
 
 
 # TODO HW : Find the best model by feature selection; best model means highest R²
+
+
+X_uni3 = select(X, :Crim) # only a single feature
+mach_uni3 = machine(model, X_uni3, y)
+fit!(mach_uni3)
+ŷ3 = MLJ.predict(mach_uni3, X_uni3)
+round(rsquared(ŷ3, y), sigdigits=4)
+
+X_uni4 = select(X, :Zn) # only a single feature
+mach_uni4 = machine(model, X_uni4, y)
+fit!(mach_uni4)
+ŷ4 = MLJ.predict(mach_uni4, X_uni4)
+round(rsquared(ŷ4, y), sigdigits=4)
+
+
+X_uni5 = select(X, :Indus) # only a single feature
+mach_uni5 = machine(model, X_uni5, y)
+fit!(mach_uni5)
+ŷ5 = MLJ.predict(mach_uni5, X_uni5)
+round(rsquared(ŷ5, y), sigdigits=4)
+
+X_uni6 = select(X, :Chas) # only a single feature
+mach_uni6 = machine(model, X_uni6, y)
+fit!(mach_uni6)
+ŷ6 = MLJ.predict(mach_uni6, X_uni6)
+round(rsquared(ŷ6, y), sigdigits=4)
+
+X_uni7 = select(X, :NOx) # only a single feature
+mach_uni7 = machine(model, X_uni7, y)
+fit!(mach_uni7)
+ŷ7 = MLJ.predict(mach_uni7, X_uni7)
+round(rsquared(ŷ7, y), sigdigits=4)
+
+X_uni8 = select(X, :Rm) # only a single feature
+mach_uni8 = machine(model, X_uni8, y)
+fit!(mach_uni8)
+ŷ8 = MLJ.predict(mach_uni8, X_uni8)
+round(rsquared(ŷ8, y), sigdigits=4)
+
+X_uni9 = select(X, :Age) # only a single feature
+mach_uni9 = machine(model, X_uni9, y)
+fit!(mach_uni9)
+ŷ9 = MLJ.predict(mach_uni9, X_uni9)
+round(rsquared(ŷ9, y), sigdigits=4)
+
+X_uni10 = select(X, :Dis) # only a single feature
+mach_uni10 = machine(model, X_uni10, y) 
+fit!(mach_uni10)
+ŷ10 = MLJ.predict(mach_uni10, X_uni10)
+round(rsquared(ŷ10, y), sigdigits=4)
+
+X_uni11 = select(X, :Rad) # only a single feature
+mach_uni11 = machine(model, X_uni11, y) 
+fit!(mach_uni11)
+ŷ11 = MLJ.predict(mach_uni11, X_uni11)
+round(rsquared(ŷ11, y), sigdigits=4)
+
+X_uni12 = select(X, :Tax) # only a single feature
+mach_uni12 = machine(model, X_uni12, y)
+fit!(mach_uni12)
+ŷ12 = MLJ.predict(mach_uni12, X_uni12)
+round(rsquared(ŷ12, y), sigdigits=4)
+
+X_uni13 = select(X, :PTRatio) # only a single feature
+mach_uni13 = machine(model, X_uni13, y) 
+fit!(mach_uni13)
+ŷ13 = MLJ.predict(mach_uni13, X_uni13)
+round(rsquared(ŷ13, y), sigdigits=4)
+
+X_uni14 = select(X, :Black) # only a single feature
+mach_uni14 = machine(model, X_uni14, y) 
+fit!(mach_uni14)
+ŷ14 = MLJ.predict(mach_uni14, X_uni14)
+round(rsquared(ŷ14, y), sigdigits=4)
+
+X_uni15 = select(X, :LStat) # only a single feature
+mach_uni15 = machine(model, X_uni15, y)
+fit!(mach_uni15)
+ŷ15 = MLJ.predict(mach_uni15, X_uni15)
+round(rsquared(ŷ15, y), sigdigits=4)
+# Now we can compare the R² values of all the models
+r_squared_values = [
+    ("Crim", round(rsquared(ŷ3, y), sigdigits=4)),
+    ("Zn", round(rsquared(ŷ4, y), sigdigits=4)),
+    ("Indus", round(rsquared(ŷ5, y), sigdigits=4)),
+    ("Chas", round(rsquared(ŷ6, y), sigdigits=4)),
+    ("NOx", round(rsquared(ŷ7, y), sigdigits=4)),
+    ("Rm", round(rsquared(ŷ8, y), sigdigits=4)),
+    ("Age", round(rsquared(ŷ9, y), sigdigits=4)),
+    ("Dis", round(rsquared(ŷ10, y), sigdigits=4)),
+    ("Rad", round(rsquared(ŷ11, y), sigdigits=4)),
+    ("Tax", round(rsquared(ŷ12, y), sigdigits=4)),
+    ("PTRatio", round(rsquared(ŷ13, y), sigdigits=4)),
+    ("B", round(rsquared(ŷ14, y), sigdigits=4)),
+    ("LStat", round(rsquared(ŷ15, y), sigdigits=4))
+]
+# find highest rsquared value in vector 
+
+X = select(data, Not(:MedV,:LStat)) # remove LStat to avoid multicollinearity)
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+
+X = select(data, Not(:MedV,:Black))
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:PTRatio))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)    
+round(rsquared(ŷ, y), sigdigits=4)
+
+
+X = select(data, Not(:MedV,:Tax))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)    
+round(rsquared(ŷ, y), sigdigits=4)
+
+
+X = select(data, Not(:MedV,:Rad))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)    
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Dis))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)    
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Age))
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Rm))
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)      
+
+X = select(data, Not(:MedV,:NOx))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Chas))
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Indus))
+mach = machine(model, X, y)
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)  
+
+X = select(data, Not(:MedV,:Zn))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+X = select(data, Not(:MedV,:Crim))
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
+
+# if we drop induc or age we have highest r_squared_values 74.06
+
+X = select(data, Not(:MedV,:Indus, :Age)) # remove Indus and Age to avoid multicollinearity
+mach = machine(model, X, y) 
+fit!(mach)
+ŷ = MLJ.predict(mach, X)
+round(rsquared(ŷ, y), sigdigits=4)
